ad89b9c3f417e387be833535db30d51561c98326
liyangli <ylisoftware@163.com>
2017-08-24 13:19:54 +0800

    调整串口发送数据后继续方式
    调整串口解析方式。增加E0错误码处理、增加解析时反馈的数据含有延长指令数据

--- serial_util.js
+++ serial_util.js
@@ -7,8 +7,9 @@
  */
 'use strict';
 const SerialPort = require('serialport');
+let dir = "";
 class SerialUtil{
-    
+
     /**
      * 类构造。进行初始化serialPort对象
      */
@@ -23,7 +24,8 @@ class SerialUtil{
             const self = this;
             ports.forEach(function(port) {
               let manufacturer = port.manufacturer;
-              if(manufacturer=='FTDI'){
+              console.info(port);
+              if(manufacturer=='Prolific'){
                   //进行设定对应数据。这个里面才是真正的串口对象
                   self.serialPort = new SerialPort(port.comName,{
                     baudRate: 9600,
@@ -32,7 +34,7 @@ class SerialUtil{
                     stopBits:1,
                     autoOpen: false
                   });
-              }  
+              }
             }, this);
 
             this.ev = ev;
@@ -47,20 +49,62 @@ class SerialUtil{
         let reviceContent = "";
         const self = this;
         let handler;
-        let timeout = 2* 1000;
-        let i = 0;
+        let commands = {'8800000088':{
+            respLen: 513*2
+        },'8A008A':{
+            respLen: 512*2
+        }};
         this.serialPort.on('data',(data)=>{
-            reviceContent += data.toString('hex');
-            if(handler){
-                clearTimeout(handler);
+            //处理步骤：
+            /**
+             * 1、接收到的数据查看是否通过指定命令方式获取到的；
+             * 2、指定命令包含对应字节个数，进行判断接收到的数据是否接收完。没有接收完成继续执行；否则截取前端数据；
+             * 3、判断首次接收到的数据是否E0.如果为E0自动退出，表明失败了。
+             */
+            const content = data.toString('hex');
+            if(reviceContent == "" && content.toLocaleLowerCase().indexOf("e0") == 0){
+                //表明为刚开始接收数据。如果为E0表明出错了。直接退出
+                console.info("reviceContent-->"+reviceContent);
+                self.ev.emit('comData',"E0");
+                reviceContent += content.substr(2);
+            }else{
+                //表明正式获取的数据。需要直接设置上去；
+                reviceContent += content;
             }
-            //此处等待2s。如果2s数据没有返回就认为是处理完成了；
-            handler = setTimeout(function(){
-                console.info(reviceContent);
-                 self.ev.emit('comData',reviceContent);
-                reviceContent = "";
-                handler = "";
-            },timeout);
+            Object.keys(commands).forEach((key)=>{
+                let keyLowCase = key.toLocaleLowerCase();
+                
+                if(reviceContent.indexOf(keyLowCase) == 0){
+                    //表明通过这种方式进行匹配上了；看对应长度是否够。不够就继续。够了就触发通知事件
+                    try{
+                        //判断指令是否重复设定了
+                        let reviceDeal = reviceContent;
+                        let reviceDealAft = reviceContent.substr(key.length);
+                        if(reviceDealAft.indexOf(keyLowCase) == 0){
+                            //表明为第二次还是原先值。需要进行把数据给截取掉然后进行计算长度是否正常；
+                            reviceDeal = reviceDealAft;
+                        }
+                        
+                        const comandVal = commands[key];
+                        const respLen = comandVal.respLen;
+                        //判断长度够不够。够了就自动截取内容并且把循环接收的数据给清除掉；
+                        let reviceLen = reviceDeal.length;
+                        console.info(`reviceLen->${reviceLen};respLen+key.length-->${respLen+key.length};ken->${key.length}`);
+                        if(reviceLen >= (respLen+key.length)){
+                            let comData = reviceDeal.substring(key.length,respLen+key.length);
+                            console.info("len-->"+comData.length);
+                            self.ev.emit('comData',comData);
+
+                            reviceContent = reviceDeal.substr(respLen+key.length);
+                        }else if(reviceLen == (key.length + 2) && reviceDeal.substr(key.length)== 'e0'){
+                            self.ev.emit('comData','e0');
+                            reviceContent = "";
+                        }
+                    }catch(e){
+                        console.error(e);
+                    }
+                }
+            });
         });
     }
 
@@ -69,11 +113,12 @@ class SerialUtil{
      */
     start(cb){
         const self = this;
-        console.info(this.serialPort);
+        // console.info(this.serialPort);
         try {
             this.serialPort.open((err)=>{
             
             if(err){
+                console.info(err);
                 cb(false,err,{});
                 return;
             }
@@ -82,6 +127,7 @@ class SerialUtil{
             cb(true,"",{});
         });
         } catch (error) {
+            console.info(error);
             cb(false,error,{});
         }
         
@@ -95,7 +141,7 @@ class SerialUtil{
         try {
             //开始进行事件绑定。如果含有问题进行抛出对应问题；
 
-            
+            dir = content;
             this.serialPort.write(new Buffer(content,'hex'),(err,results)=>{
             //需要进行等待接收完毕然后进行返回数据；
             if(err){
